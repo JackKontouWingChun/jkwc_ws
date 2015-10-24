99e6d56848f2bfdf5136c1010f25b87f17361fa8
JackKontouWingChun <paul@jkwc.co.uk>
2015-10-24 14:29:21 +0100

    multiple layout, structure changes
    - changes to map and inclusion of lighter map styles
    - order of sections changed

--- js/grayscale.js
+++ js/grayscale.js
@@ -29,240 +29,121 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-/*
-// Google Maps Scripts
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
-}
-*/
-
 // MY GOOGLE MAPS FROM MAPBUILDER
 google.maps.event.addDomListener(window, 'load', init);
 var map;
 function init() {
-	var mapOptions = {
-		center: new google.maps.LatLng(51.508215,-0.269872),
-		zoom: 17,
-		zoomControl: true,
-		zoomControlOptions: {
-			style: google.maps.ZoomControlStyle.SMALL,
-		},
-		disableDoubleClickZoom: true,
-		mapTypeControl: true,
-		mapTypeControlOptions: {
-			style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-		},
-		scaleControl: true,
-		scrollwheel: false,
-		panControl: true,
-		streetViewControl: true,
-		draggable : true,
-		overviewMapControl: true,
-		overviewMapControlOptions: {
-			opened: true,
-		},
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},
-				 {"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},
-				 {"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-				 {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},
-				 {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},
-				 {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},
-				 {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},
-				 {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},
-				 {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},
-				 {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},
-				 {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},
-				 {"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},
-				 {"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}],
-	}
-	var mapElement = document.getElementById('jkwc');
-	var map = new google.maps.Map(mapElement, mapOptions);
-	var locations = [
-		['108 Churchfield Road', 'undefined', 'undefined', 'info@jkwc.co.uk', 'www.jkwc.co.uk', 51.5084119, -0.26942210000004, 'https://mapbuildr.com/assets/img/markers/hollow-pin-yellow.png']
-	];
-	for (i = 0; i < locations.length; i++) {
-		if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-		if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-		if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-		if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-		if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-		marker = new google.maps.Marker({
-			icon: markericon,
-			position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-			map: map,
-			title: locations[i][0],
-			desc: description,
-			tel: telephone,
-			email: email,
-			web: web
-		});
-		link = '';
-		bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
-	 }
+        var mapOptions = {
+            center: new google.maps.LatLng(51.507273,-0.269545),
+            zoom: 15,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL,
+            },
+            disableDoubleClickZoom: true,
+            mapTypeControl: false,
+            scaleControl: true,
+            scrollwheel: false,
+            panControl: false,
+            streetViewControl: false,
+            draggable : true,
+            overviewMapControl: false,
+            overviewMapControlOptions: {
+                opened: false,
+            },
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+			styles: [
+				/* LIGHT GREY
+				{"featureType": "water","elementType": "geometry.fill","stylers": [{ "color": "#d3d3d3" }]},
+				{"featureType": "transit","stylers": [{ "color": "#808080" },{ "visibility": "off" }]},
+				{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{ "visibility": "on" },{ "color": "#b3b3b3" }]},
+				{"featureType": "road.highway","elementType": "geometry.fill","stylers": [{ "color": "#ffffff" }]},
+				{"featureType": "road.local","elementType": "geometry.fill","stylers": [{ "visibility": "on" },{ "color": "#ffffff" },{ "weight": 1.8 }]},
+				{"featureType": "road.local","elementType": "geometry.stroke","stylers": [{ "color": "#d7d7d7" }]},
+				{"featureType": "poi","elementType": "geometry.fill","stylers": [{ "visibility": "on" },{ "color": "#ebebeb" }]},
+				{"featureType": "administrative","elementType": "geometry","stylers": [{ "color": "#a7a7a7" }]},{"featureType": "road.arterial","elementType": "geometry.fill","stylers": [{ "color": "#ffffff" }]},
+				{"featureType": "road.arterial","elementType": "geometry.fill","stylers": [{ "color": "#ffffff" }]},
+				{"featureType": "landscape","elementType": "geometry.fill","stylers": [{ "visibility": "on" },{ "color": "#efefef" }]},
+				{"featureType": "road","elementType": "labels.text.fill","stylers": [{ "color": "#696969" }]},
+				{"featureType": "administrative","elementType": "labels.text.fill","stylers": [{ "visibility": "on" },{ "color": "#737373" }]},
+				{"featureType": "poi","elementType": "labels.icon","stylers": [{ "visibility": "off" }]},
+				{"featureType": "poi","elementType": "labels","stylers": [{ "visibility": "off" }]},
+				{"featureType": "road.arterial","elementType": "geometry.stroke","stylers": [{ "color": "#d6d6d6" }]},
+				{"featureType": "road","elementType": "labels.icon","stylers": [{ "visibility": "off" }]},
+				{ },
+				{"featureType": "poi","elementType": "geometry.fill","stylers": [{ "color": "#dadada" }]}
+				*/
+				{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},
+				{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},
+				{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
+				{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},
+				{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},
+				{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},
+				{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},
+				{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},
+				{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},
+				{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},
+				{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},
+				{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},
+				{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}
+			],
+        }
+        var mapElement = document.getElementById('jkwc');
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var locations = [
+			[	'Jack Kontou Wing Chun',
+				'108 Churchfield Road,<br/>Acton,<br>W3 6BY',
+				'undefined',
+				'undefined',
+				'undefined',
+				51.508412, 
+				-0.269422,
+				'https://mapbuildr.com/assets/img/markers/solid-pin-yellow.png'
+			]
+        ];
+        for (i = 0; i < locations.length; i++) {
+			if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+			if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+			if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+			if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+			if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+            marker = new google.maps.Marker({
+                icon: markericon,
+                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+                map: map,
+                title: locations[i][0],
+                desc: description,
+                tel: telephone,
+                email: email,
+                web: web
+            });
+			if (web.substring(0, 7) != "http://") {
+				link = "http://" + web;
+			} else {
+				link = web;
+			}
+            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+		}
 	function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
 		var infoWindowVisible = (function () {
 			var currentlyVisible = false;
 			return function (visible) {
-				if (visible !== undefined) {
-					currentlyVisible = visible;
-				}
-				return currentlyVisible;
+			if (visible !== undefined) {
+			currentlyVisible = visible;
+			}
+			return currentlyVisible;
 			};
 		}());
 		iw = new google.maps.InfoWindow();
 		google.maps.event.addListener(marker, 'click', function() {
 			if (infoWindowVisible()) {
-				iw.close();
-				infoWindowVisible(false);
+			iw.close();
+			infoWindowVisible(false);
 			} else {
-				var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h6>"+title+"</h6><p>"+desc+"<p></div>";
-				iw = new google.maps.InfoWindow({content:html});
-				iw.open(map,marker);
-				infoWindowVisible(true);
+			var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>"+title+"</h4><p>"+desc+"<p><p>"+telephone+"<p><a href='mailto:"+email+"' >"+email+"<a><a href='"+link+"'' >"+web+"<a></div>";
+			iw = new google.maps.InfoWindow({content:html});
+			iw.open(map,marker);
+			infoWindowVisible(true);
 			}
 		});
 		google.maps.event.addListener(iw, 'closeclick', function () {
